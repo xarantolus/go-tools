76d2542fd1aa0b1981f6d68a53779fa0ecb3eff4
Philipp Erhardt <philipperhardt29@gmail.com>
2018-07-08 13:38:19 +0200

    Remove 'console.log' statements

--- assets/browser.js
+++ assets/browser.js
@@ -3,12 +3,7 @@ var goStartDate = dayjs("2006-01-02 15:04:05");
 
 function dateInput() {
     var newFormat = document.getElementById('date-left').value;
-    console.log(newFormat);
-
-    console.log(goStartDate.format(newFormat));
-
     document.getElementById('date-right').value = goStartDate.format(newFormat).toString();
-
 }
 
 
