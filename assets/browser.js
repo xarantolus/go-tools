a2f3c9291eadf6ed9511beb6dc97ff52c29ad4d5
Philipp Erhardt <philipperhardt29@gmail.com>
2018-07-10 12:05:53 +0200

    Add footer & show warning when using an invalid date format

--- assets/browser.js
+++ assets/browser.js
@@ -3,7 +3,16 @@ var goStartDate = dayjs("2006-01-02 15:04:05");
 
 function dateInput() {
     var newFormat = document.getElementById('date-left').value;
-    document.getElementById('date-right').value = goStartDate.format(newFormat).toString();
+
+    var warn = document.getElementById('date-warning-dd');
+    if (newFormat.indexOf("dd") != -1 && newFormat.indexOf("ddd") == -1) {
+        // Show day formatting warning to user
+        warn.style.display = "block";
+    } else {
+        // Everything is fine, show the format
+        warn.style.display = "none";
+        document.getElementById('date-right').value = goStartDate.format(newFormat).toString();
+    }
 }
 
 
